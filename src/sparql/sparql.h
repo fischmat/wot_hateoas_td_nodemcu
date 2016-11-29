294914eecd80c3f798806d47c2573340996ac9cf
fischmat <mail@matthias-fisch.de>
2016-11-29 22:14:37 +0100

    Function for querying equivalent classes with SPARQL.

---
+++ src/sparql/sparql.h
@@ -0,0 +1,10 @@
+#ifndef __WOT_SPARQL__
+#define __WOT_SPARQL__
+
+namespace wot {
+
+unsigned queryEquivalentClasses(const char *iri, char **out, unsigned char entryCount, unsigned entryLen);
+
+}
+
+#endif
