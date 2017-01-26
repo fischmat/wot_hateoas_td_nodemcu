304a43ae974f742006e82653356fb685a1ecadb9
Matthias Fisch <mail@matthias-fisch.de>
2017-01-26 23:46:39 +0100

    Resolver for handling shorthand notations of IRIs

--- src/sparql/sparql.cpp
+++ src/sparql/sparql.cpp
@@ -2,8 +2,79 @@
 #include <ESP8266WiFi.h>
 #include "../ajson/aJSON.h"
 #include "../util.h"
+#include <string.h>
 
 using namespace wot;
+
+wot::PrefixResolver::PrefixResolver()
+    : numPrefixes(0) {}
+
+wot::PrefixResolver::~PrefixResolver()
+{
+    for(unsigned char i = 0; i < numPrefixes; i++)
+    {
+        delete[] prefixes[i].name;
+        delete[] prefixes[i].prefix;
+    }
+}
+
+bool wot::PrefixResolver::addPrefix(const char *name, const char *prefix)
+{
+    if(numPrefixes < 10)
+    {
+        unsigned nameLen = strlen(name) + 1;
+        unsigned prefixLen = strlen(prefix) + 1;
+        prefixes[numPrefixes].name = new char[nameLen];
+        prefixes[numPrefixes].prefix = new char[prefixLen];
+        strcpy_s(prefixes[numPrefixes].name, nameLen, name);
+        strcpy_s(prefixes[numPrefixes].prefix, prefixLen, prefix);
+        numPrefixes++;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+unsigned wot::PrefixResolver::resolve(const char *in, char *out, unsigned outLen)
+{
+    unsigned inLen = strlen(in) + 1;
+    char *copy = new char[inLen];
+    strcpy_s(copy, inLen, in);
+
+    char *suffix = NULL;
+    for(unsigned i = 0; i < inLen; i++) {
+        if(copy[i] == ':') {
+            copy[i] = '\0';
+            suffix = copy + i + 1;
+        }
+    }
+    if(suffix)
+    {
+        unsigned suffixLen = strlen(suffix);
+
+        char *prefix = NULL;
+        for(unsigned char i = 0; i < numPrefixes; i++) {
+            if(!strcmp(prefixes[i].name, copy)) {
+                prefix = prefixes[i].prefix;
+            }
+        }
+
+        if(prefix)
+        {
+            unsigned prefixLen = strlen(prefix);
+            if(prefixLen + suffixLen < outLen) {
+                strcpy_s(out, outLen, prefix);
+                strcpy_s(out + prefixLen, outLen - prefixLen, suffix);
+                delete[] copy;
+                return prefixLen + suffixLen;
+            }
+        }
+    }
+    delete[] copy;
+    strcpy_s(out, outLen, in); // In last instance go with the input
+    return 0;
+}
 
 const char *equivClassesQuery[] = {
     "PREFIX%20rdfs%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0APREFIX%20owl%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0ASELECT%20%3Fc%20%7B%0A%20%20%7B%20%3C",
@@ -23,10 +94,12 @@ inline void readUntilPayload(WiFiClient *client)
     } while(!payloadFound);
 }
 
-unsigned short wot::queryEquivalentClasses(const char *iri, char **out, unsigned char entryCount, unsigned entryLen)
+unsigned short wot::queryEquivalentClasses(const char *iri, char **out,
+                                    unsigned char entryCount, unsigned entryLen,
+                                    const char *endpointHost, const char *endpointPath)
 {
     WiFiClient con;
-    if(con.connect("lov.okfn.org", 80)) {
+    if(con.connect(endpointHost, 80)) {
         // Construct the urlencoded payload of the request:
         String data("query=");
         data += equivClassesQuery[0];
@@ -40,8 +113,8 @@ unsigned short wot::queryEquivalentClasses(const char *iri, char **out, unsigned
         data += equivClassesQuery[4];
 
         // Make a HTTP POST-request:
-        con.print("POST /dataset/lov/sparql HTTP/1.1\r\n");
-        con.print("Host: lov.okfn.org\r\n");
+        con.printf("POST %s HTTP/1.1\r\n", endpointPath);
+        con.printf("Host: %s\r\n", endpointHost);
         con.print("Content-Type: application/x-www-form-urlencoded; charset=UTF-8\r\n");
         con.print("Accept: application/sparql-results+json,*/*;q=0.9\r\n");
         con.printf("Content-Length: %d\r\n\r\n", data.length());
@@ -86,4 +159,27 @@ unsigned short wot::queryEquivalentClasses(const char *iri, char **out, unsigned
         }
     }
     return -1;
+}
+
+bool wot::classesEquivalent(const char *iri1, const char *iri2)
+{
+    // Prepare array of equivalent classes:
+    char **eq = new char*[10];
+    for(unsigned char j = 0; j < 10; j++) {
+        eq[j] = new char[200];
+    }
+    unsigned short numEqClasses = queryEquivalentClasses(iri1, eq, 10, 200);
+
+    // Check if any of those equivalent IRIs is the same as the second:
+    bool foundEquivalent = false;
+    for(unsigned short j = 0; j < numEqClasses; j++) {
+        if(!strcmp(eq[j], iri2)) {
+            foundEquivalent = true;
+        }
+    }
+    for(unsigned char j = 0; j < 10; j++) {
+        delete[] eq[j];
+    }
+    delete[] eq;
+    return foundEquivalent;
 }
--- src/sparql/sparql.h
+++ src/sparql/sparql.h
@@ -1,11 +1,40 @@
 #ifndef __WOT_SPARQL__
 #define __WOT_SPARQL__
 
+#define DEFAULT_SPARQL_ENDPOINT_HOST "lov.okfn.org"
+#define DEFAULT_SPARQL_ENDPOINT_PATH "/dataset/lov/sparql"
+
 namespace wot {
+
+typedef struct {
+    char *name;
+    char *prefix;
+} Prefix;
+
+class PrefixResolver
+{
+private:
+    Prefix prefixes[10];
+    unsigned char numPrefixes;
+public:
+    PrefixResolver();
+    ~PrefixResolver();
+
+    /**
+    * Adds a prefix definition to this resolver.
+    * name: The shorthand for the prefix.
+    * prefix: The IRI prefix the shorthand stands for.
+    * Returns true if the definition was successfully inserted.
+    * False if the maximum count was reached.
+    */
+    bool addPrefix(const char *name, const char *prefix);
+
+    unsigned resolve(const char *shorthand, char *out, unsigned outLen);
+};
 
 /**
 * Retrieves equivalent classes and/or resources for a given class or resource.
-* Queries the SPARQL-endpoint at lov.okfn.org and returns the IRIs in the transitive closure of rdfs:seeAlso and owl:sameAs.
+* Queries the SPARQL-endpoint specified and returns the IRIs in the transitive closure of rdfs:seeAlso and owl:sameAs.
 *
 * iri: The IRI of the class/resource for which equivalent classes/resources should be determined.
 * out: The equivalent IRIs will be written at the first addresses pointed to by this array.
@@ -14,7 +43,12 @@ namespace wot {
 *
 * Returns the number of results written to out on success. Returns negative on error.
 */
-unsigned short queryEquivalentClasses(const char *iri, char **out, unsigned char entryCount, unsigned entryLen);
+unsigned short queryEquivalentClasses(const char *iri, char **out,
+                                unsigned char entryCount, unsigned entryLen,
+                                const char *endpointHost = DEFAULT_SPARQL_ENDPOINT_HOST,
+                                const char *endpointPath = DEFAULT_SPARQL_ENDPOINT_PATH);
+
+bool classesEquivalent(const char *iri1, const char *iri2);
 
 }
 
